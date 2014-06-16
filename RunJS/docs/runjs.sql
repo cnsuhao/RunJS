016cec6337ff00b626a8a7cccd918039bb8bec6e
红薯 <ld@oschina.net>
2014-06-17 07:53:32 +0800

    Update runjs.sql

--- RunJS/docs/runjs.sql
+++ RunJS/docs/runjs.sql
@@ -1,22 +1,37 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : oschina
-Source Server Version : 50520
-Source Host           : 192.168.1.11:3306
-Source Database       : runjs
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50614
+ Source Host           : localhost
+ Source Database       : runjs
 
-Target Server Type    : MYSQL
-Target Server Version : 50520
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50614
+ File Encoding         : utf-8
 
-Date: 2012-10-11 09:49:30
+ Date: 06/17/2014 07:52:18 AM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `osc_codes`
+--  Table structure for `osc_catalogs`
+-- ----------------------------
+DROP TABLE IF EXISTS `osc_catalogs`;
+CREATE TABLE `osc_catalogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `parent` int(11) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1053 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `osc_codes`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_codes`;
 CREATE TABLE `osc_codes` (
@@ -36,14 +51,19 @@ CREATE TABLE `osc_codes` (
   `description` mediumtext,
   `post_time` timestamp NULL DEFAULT NULL,
   `sign` varchar(32) DEFAULT NULL,
+  `view_count` int(11) DEFAULT '0',
+  `catalog` int(11) DEFAULT '0',
+  `code_type` int(11) DEFAULT '111',
   PRIMARY KEY (`id`),
-  KEY `idx_code_project` (`project`),
-  KEY `idx_code_ident` (`ident`),
-  KEY `idx_code_fork` (`fork`)
-) ENGINE=InnoDB AUTO_INCREMENT=793 DEFAULT CHARSET=utf8;
+  KEY `idx_code_project` (`project`) USING BTREE,
+  KEY `idx_code_ident` (`ident`) USING BTREE,
+  KEY `idx_code_fork` (`fork`) USING BTREE,
+  KEY `idx_code_catalog` (`catalog`) USING BTREE,
+  KEY `idx_code_user` (`user`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=71387 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_comments`
+--  Table structure for `osc_comments`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_comments`;
 CREATE TABLE `osc_comments` (
@@ -53,11 +73,26 @@ CREATE TABLE `osc_comments` (
   `content` text,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `idx_comment_code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+  KEY `idx_comment_code` (`code`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4164 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_favors`
+--  Table structure for `osc_dynamics`
+-- ----------------------------
+DROP TABLE IF EXISTS `osc_dynamics`;
+CREATE TABLE `osc_dynamics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `type` int(11) DEFAULT NULL,
+  `refer` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_dy_user` (`user`) USING BTREE,
+  KEY `idx_dy_type` (`type`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=84695 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `osc_favors`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_favors`;
 CREATE TABLE `osc_favors` (
@@ -71,11 +106,7 @@ CREATE TABLE `osc_favors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of osc_favors
--- ----------------------------
-
--- ----------------------------
--- Table structure for `osc_files`
+--  Table structure for `osc_files`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_files`;
 CREATE TABLE `osc_files` (
@@ -88,11 +119,11 @@ CREATE TABLE `osc_files` (
   `ident` varchar(32) DEFAULT NULL,
   `type` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_file_user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
+  KEY `idx_file_user` (`user`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=10910 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_login_bindings`
+--  Table structure for `osc_login_bindings`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_login_bindings`;
 CREATE TABLE `osc_login_bindings` (
@@ -105,11 +136,58 @@ CREATE TABLE `osc_login_bindings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of osc_login_bindings
+--  Table structure for `osc_msgs`
 -- ----------------------------
+DROP TABLE IF EXISTS `osc_msgs`;
+CREATE TABLE `osc_msgs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
+  `content` varchar(256) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `refer` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_msg_receiver` (`receiver`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=53129 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_projects`
+--  Table structure for `osc_plugin_codes`
+-- ----------------------------
+DROP TABLE IF EXISTS `osc_plugin_codes`;
+CREATE TABLE `osc_plugin_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codeid` int(11) DEFAULT NULL,
+  `html` mediumtext,
+  `js` mediumtext,
+  `css` mediumtext,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_plugin_code` (`codeid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `osc_plugins`
+-- ----------------------------
+DROP TABLE IF EXISTS `osc_plugins`;
+CREATE TABLE `osc_plugins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `code` int(11) DEFAULT NULL,
+  `user` int(11) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_plugin_user` (`user`) USING BTREE,
+  KEY `idx_plugin_status` (`status`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1130 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `osc_projects`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_projects`;
 CREATE TABLE `osc_projects` (
@@ -121,41 +199,43 @@ CREATE TABLE `osc_projects` (
   `version` int(11) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_project_user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=274 DEFAULT CHARSET=utf8;
+  KEY `idx_project_user` (`user`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=70858 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_settings`
+--  Table structure for `osc_settings`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_settings`;
 CREATE TABLE `osc_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL,
   `name` varchar(32) DEFAULT NULL,
-  `value` varchar(32) DEFAULT NULL,
+  `value` varchar(1024) DEFAULT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `idx_setting_user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+  KEY `idx_setting_user` (`user`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6060 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of osc_settings
+--  Table structure for `osc_square_codes`
 -- ----------------------------
-INSERT INTO `osc_settings` VALUES ('1', '1', 'theme', 'rubyblue', '2012-09-10 17:38:49');
-INSERT INTO `osc_settings` VALUES ('2', '3', 'theme', 'night', '2012-10-10 17:00:54');
-INSERT INTO `osc_settings` VALUES ('3', '4', 'theme', 'default', '2012-09-28 16:40:01');
-INSERT INTO `osc_settings` VALUES ('4', '16', 'theme', 'default', '2012-09-26 13:42:46');
-INSERT INTO `osc_settings` VALUES ('5', '5', 'theme', 'default', '2012-09-14 12:05:06');
-INSERT INTO `osc_settings` VALUES ('6', '7', 'theme', 'eclipse', '2012-09-19 14:33:13');
-INSERT INTO `osc_settings` VALUES ('8', '19', 'theme', 'night', '2012-10-08 14:17:04');
-INSERT INTO `osc_settings` VALUES ('9', '33', 'theme', 'night', '2012-10-09 11:45:39');
-INSERT INTO `osc_settings` VALUES ('10', '18', 'theme', 'default', '2012-10-08 18:02:58');
-INSERT INTO `osc_settings` VALUES ('11', '29', 'theme', 'night', '2012-10-08 14:22:06');
-INSERT INTO `osc_settings` VALUES ('12', '30', 'theme', 'default', '2012-10-08 15:33:05');
-INSERT INTO `osc_settings` VALUES ('13', '25', 'theme', 'default', '2012-10-10 14:54:12');
+DROP TABLE IF EXISTS `osc_square_codes`;
+CREATE TABLE `osc_square_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codeid` int(11) DEFAULT NULL,
+  `html` mediumtext,
+  `js` mediumtext,
+  `css` mediumtext,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `code_type` int(11) DEFAULT '111',
+  PRIMARY KEY (`id`),
+  KEY `idx_square_code` (`codeid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=472 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_users`
+--  Table structure for `osc_users`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_users`;
 CREATE TABLE `osc_users` (
@@ -174,11 +254,26 @@ CREATE TABLE `osc_users` (
   `space_url` varchar(64) DEFAULT NULL,
   `blog` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_user_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
+  KEY `idx_user_email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=27794 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for `osc_votes`
+--  Table structure for `osc_visit_stats`
+-- ----------------------------
+DROP TABLE IF EXISTS `osc_visit_stats`;
+CREATE TABLE `osc_visit_stats` (
+  `stat_date` int(10) unsigned NOT NULL,
+  `type` tinyint(4) NOT NULL,
+  `id` int(10) unsigned NOT NULL,
+  `view_count` int(11) NOT NULL,
+  PRIMARY KEY (`stat_date`,`type`,`id`),
+  KEY `idx_visit_stat_id` (`id`) USING BTREE,
+  KEY `idx_visit_stat_date` (`stat_date`) USING BTREE,
+  KEY `idx_visit_stat_type` (`type`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `osc_votes`
 -- ----------------------------
 DROP TABLE IF EXISTS `osc_votes`;
 CREATE TABLE `osc_votes` (
@@ -188,5 +283,7 @@ CREATE TABLE `osc_votes` (
   `code` int(11) DEFAULT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `idx_vote_code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  KEY `idx_vote_code` (`code`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5764 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
