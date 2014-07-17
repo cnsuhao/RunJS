955906239c0fcabeaa72d77b04fc5e8074a00665
Winter Lau <ld@oschina.net>
2014-07-17 09:20:22 +0800

    build

--- RunJS/build.sh
+++ RunJS/build.sh
@@ -1,5 +1,5 @@
 ## auto build script
-export RUNJS_HOME=/home/runjs/runjs
-export JAVA_HOME=/home/runjs/jdk1.7.0_10
-cd $RUNJS_HOME
-$JAVA_HOME/bin/java -cp "$RUNJS_HOME/packages/ant-launcher.jar:$RUNJS_HOME/packages/ant.jar:$JAVA_HOME/lib/tools.jar" org.apache.tools.ant.launch.Launcher -lib $RUNJS_HOME/packages $*
+#export RUNJS_HOME=/home/runjs/runjs
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+
+java -cp "packages/ant-launcher.jar:packages/ant.jar:$JAVA_HOME/lib/tools.jar" org.apache.tools.ant.launch.Launcher -lib $RUNJS_HOME/packages $*
