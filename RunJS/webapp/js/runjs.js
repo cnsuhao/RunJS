a16b6d94a3836e4c95a3fb1da8a2609895953216
lxbzmy <lxbzmy@gmail.com>
2014-07-17 17:02:10 +0800

    忘记try catch

--- RunJS/webapp/js/runjs.js
+++ RunJS/webapp/js/runjs.js
@@ -675,7 +675,11 @@ RunJS = (function() {
             instance.editor.initView(ident, async);
         instance.explorer.initView(true, ident, async);
         instance.menu.initView(ident, async);
-        instance.plugins.init();
+        try{
+            instance.plugins.init();
+        }catch(e){
+            Console.log(e);
+        }
     }
 
     /**
